731cc73632ad7665d402e78a90bcb783619ed94a
Owen Prendergast <prendergasto@gmail.com>
2020-03-10 09:17:37 -0700

    more code challenge

--- challenges/day1.js
+++ challenges/day1.js
@@ -3,5 +3,13 @@ function map(array, func) {
     for (let i = 0; i < array.length; i++) {
         result[i] = func(array[i]);
     }
+
     return result;
 }
+
+function double(n) {
+    return n * 2;
+}
+
+console.log(map([1, 2, 3, 4], double))
+
