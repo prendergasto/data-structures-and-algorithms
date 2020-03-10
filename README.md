3267c486b0d6443e6ade383a62debaaadc7939f8
Owen Prendergast <prendergasto@gmail.com>
2020-03-10 09:09:42 -0700

    made a function called map

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # data-structures-and-algorithms
+
+Directory:
+
+1. Code challenge #1 --> challenges --> arrayMap --> array-map.js
---
+++ challenges/day1.js
@@ -0,0 +1,7 @@
+function map(array, func) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        result[i] = func(array[i]);
+    }
+    return result;
+}
